6e1dff82c3044780ab7588c9a996d606c7d00d6d
Julette Peque <22-0200c@sgen.edu.ph>
2023-01-10 09:24:25 +0800

    Created UI and Upload Functionality

--- fileUploadTuts/fileUploadTuts/MainWindow.xaml.cs
+++ fileUploadTuts/fileUploadTuts/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace fileUploadTuts
 {
@@ -20,9 +22,58 @@ namespace fileUploadTuts
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string filePath = string.Empty;
+        private string fileServerLocation = string.Empty;
+        private string fileName = string.Empty;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void fileBrowseBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            string[] temp = new string[] { };
+            
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "*.txt | (*.txt)";
+            ofd.DefaultExt= "txt";
+            ofd.InitialDirectory = " ";
+            ofd.Title = "Uploading File";
+
+            if ((bool)ofd.ShowDialog() != true) 
+            {
+                return;
+            }
+
+            temp = ofd.FileName.Split('\\');
+            fileName = temp[temp.Length];
+            filePath = ofd.FileName;
+            fileNameLbl.Content = filePath;
+        }
+
+        private void submitFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (filePath.Length < 1) 
+            {
+                MessageBox.Show("No file has been browsed!");
+                return;
+            }
+
+            fileServerLocation = "C:\\Users\\julet\\Documents\\";
+
+            Directory.CreateDirectory(fileServerLocation);
+
+            try
+            {
+                File.Copy(filePath, fileServerLocation + "\\" + fileName);
+            }
+            catch (IOException ioe) 
+            {
+                MessageBox.Show(ioe.Message);
+            }
+
         }
     }
 }
